18ef87dc155b2d40cd198da7eaead91b51b7b12f
Rushabh A. Sarvaiya <sarvaiya.rushabh@gmail.com>
2022-10-31 22:29:12 +0530

    taskTWo Completed

--- taskTwo.js
+++ taskTwo.js
@@ -4,36 +4,51 @@ let jobsFile = './data/jobs.csv';
 let reactionsFile = './data/reactions.csv';
 
 let jobs = csvToJson.fieldDelimiter(',').getJsonFromCsv(jobsFile);
-console.log(jobs[0]);
-let jobs1 = [...jobs];
+// console.log(jobs[0]);
 let reactions = csvToJson.fieldDelimiter(',').getJsonFromCsv(reactionsFile);
-
 reactions = reactions.filter(x => x.direction === 'true');
-// let reactions1 = [...reactions];
-console.log(reactions[0]);
+// console.log(reactions[0]);
 //Task 2
-let jobCount = {};
-for (let i = 0; i < reactions.length; i++) {
-    if (!jobCount.hasOwnProperty(reactions[i].user_id)) {
-        jobCount[reactions[i].user_id] = [reactions[i].job_id];
-    } else {
-        if (!jobCount[reactions[i].user_id].includes(reactions[i].job_id))
-            jobCount[reactions[i].user_id].push(reactions[i].job_id);
-    }
+let jobCmpMap = {};
+for (let i = 0; i < jobs.length; i++) {
+    // console.log(`${jobs[i].job_id}:${jobs[i].company_id}`)
+    jobCmpMap[jobs[i].job_id] = jobs[i].company_id;
 }
-console.log(`jobCount: ${jobCount["5193"]}`);
+
+// console.log(`jobCmpMap: ${jobCmpMap["9975"]}`);
+// console.log(`jobCmpMap: ${JSON.stringify(jobCmpMap)}`);
 let cmpUserMap = {};
-for (const user in jobCount) {
-    for (let i = 0; i < jobCount[user].length; i++) {
-    let currJob = jobs.filter(x => x === user[i])
-    if (!cmpUserMap.hasOwnProperty(currJob.company_id)) {
-        cmpUserMap[currJob.company_id] = user
+for (let i = 0; i < reactions.length; i++) {
+    if (!cmpUserMap.hasOwnProperty(jobCmpMap[reactions[i].job_id])) {
+        cmpUserMap[jobCmpMap[reactions[i].job_id]] = [reactions[i].user_id];
     } else {
-        if (!jobCount[reactions[i].user_id].includes(reactions[i].job_id))
-            jobCount[reactions[i].user_id].push(reactions[i].job_id);
-    }
+        if (!cmpUserMap[jobCmpMap[reactions[i].job_id]].includes(reactions[i].user_id))
+            cmpUserMap[jobCmpMap[reactions[i].job_id]].push(reactions[i].user_id);
     }
 }
+// console.log(`cmpUserMap: ${JSON.stringify(cmpUserMap)}`);
+function countSimilarScore(a, b) {
+    let count = 0;
+    for (let i = 0; i < a.length; i++) {
+        if (b.includes(a[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+
+let allCmps = Object.keys(cmpUserMap);
 let cmpSimilarityScore = {};
-// let maxSimilarity = Object.keys(cmpSimilarityScore).reduce(function (a, b) { return cmpSimilarityScore[a] > cmpSimilarityScore[b] ? a : b });
-// console.log(`Max Similarity score is of users:${maxSimilarity.split("_")} and score is ${cmpSimilarityScore[maxSimilarity]}`);
+for (let i = 0; i < allCmps.length; i++) {
+    for (let j = 0; j != i && j < allCmps.length; j++) {
+        let cmpStr = allCmps[i] + "_" + allCmps[j];
+        let cmpStrrev = allCmps[j] + "_" + allCmps[i];
+        if (!cmpSimilarityScore.hasOwnProperty(cmpStr) && !cmpSimilarityScore.hasOwnProperty(cmpStrrev)) {
+            // cmpSimilarityScore[cmpStr] = countSimilarScore(cmpUserMap[allCmps[i]], cmpUserMap[allCmps[j]]);
+            cmpSimilarityScore[cmpStr] = countSimilarScore(cmpUserMap[allCmps[i]], cmpUserMap[allCmps[j]]);
+        }
+    }
+}
+
+let maxSimilarity = Object.keys(cmpSimilarityScore).reduce(function (a, b) { return cmpSimilarityScore[a] > cmpSimilarityScore[b] ? a : b });
+console.log(`Max Similarity score is of companies: ${maxSimilarity.split("_")} and score is ${cmpSimilarityScore[maxSimilarity]}`);
